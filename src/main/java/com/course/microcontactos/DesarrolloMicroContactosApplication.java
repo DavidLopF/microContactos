c40e3453af20c379813e3c62335429135259d7bb
David <david.lopez@puntored.co>
2023-05-18 12:57:17 -0500

    frist commit

---
+++ src/main/java/com/course/microcontactos/DesarrolloMicroContactosApplication.java
@@ -0,0 +1,13 @@
+package com.course.microcontactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloMicroContactosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesarrolloMicroContactosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/microcontactos/DesarrolloMicroContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.microcontactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloMicroContactosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
